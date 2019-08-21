730253d39edc4d493bf1b02e8588260aeb28ae33
Billy Beck <billys.beck@gmail.com>
2019-08-21 14:32:21 -0400

    Updates

---
+++ precache-manifest.8d50d6ec0843da82e55fd699157223d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8d3160cd39a705a1014beda5b29486d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c1786f71db66eb1f9b17",
+    "url": "/monsters-rolodex/static/css/main.188ae457.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "c1786f71db66eb1f9b17",
+    "url": "/monsters-rolodex/static/js/main.5ca53ac8.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
