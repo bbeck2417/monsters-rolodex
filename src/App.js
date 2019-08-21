1e3d0e63e5a24453676b56ed70f96c09aab73148
Billy Beck <billys.beck@gmail.com>
2019-08-21 14:36:58 -0400

    Updating SearchBox placeholder

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ class App extends Component {
     <div className="App">
     <h1> Monsters Rolodex </h1>
     <SearchBox 
-      placeholder='Search Monsters!!!'
+      placeholder='Search the Monsters!!!'
       handleChange={ this.handleChange}
     />
 
